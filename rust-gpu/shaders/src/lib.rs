35ab150bc0d4804fe443b2c5825de685d928046d
konyogony <me@konyogony.dev>
2026-04-24 23:25:52 +0700

    Rough version of vector arrows using sdf's

--- rust-gpu/shaders/src/lib.rs
+++ rust-gpu/shaders/src/lib.rs
@@ -7,6 +7,8 @@
 use bytemuck::{Pod, Zeroable};
 use glam::{Vec2, Vec4, Vec4Swizzles};
 use spirv_std::arch::Derivative;
+// DONT REMOVE
+use spirv_std::num_traits::Float;
 use spirv_std::spirv;
 
 const GRID_COLOR: Vec4 = Vec4::new(0.3, 0.3, 0.3, 0.05);
@@ -14,6 +16,7 @@ const AXIS_COLOR: Vec4 = Vec4::new(1.0, 1.0, 1.0, 0.8);
 const BG_COLOR: Vec4 = Vec4::new(0.0, 0.0, 0.0, 1.0);
 const HIGHLIGHT_COLOR: Vec4 = Vec4::new(0.0, 1.0, 1.0, 0.4);
 const THICKNESS: f32 = 1.0;
+const ARROW_THICKNESS: f32 = 2.0;
 const GRID_SPACING: f32 = 30.0;
 const HIGHLIGHT_SQUARES: f32 = 3.0;
 
@@ -37,13 +40,8 @@ pub fn main_vs(#[spirv(vertex_index)] vert_id: i32, #[spirv(position)] vtx_pos: 
     *vtx_pos = pos.extend(0.0).extend(1.0);
 }
 
-#[repr(u32)]
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum Status {
-    GridLine,
-    AxisLine,
-    HighlightLine,
-    None,
+pub fn arrow_fn(x: f32, y: f32) -> Vec2 {
+    Vec2::new(10.0, 20.0)
 }
 
 #[spirv(fragment)]
@@ -53,7 +51,6 @@ pub fn main_fs(
     output: &mut Vec4,
 ) {
     // Store the initial rgb values.
-    let output_color = BG_COLOR.xyz();
 
     // Center first
     let uv = frag_coords / Vec4::new(constants.width as f32, constants.height as f32, 0.0, 0.0);
@@ -61,37 +58,85 @@ pub fn main_fs(
 
     // Convert to pixels
     let px_x = centered_uv.x * constants.width as f32;
-    let px_y = centered_uv.y * constants.height as f32;
+    let px_y = -centered_uv.y * constants.height as f32;
 
-    // Get how many times spacing wraps.
-    let grid_distance_x = (px_x % GRID_SPACING).abs();
-    let grid_distance_y = (px_y % GRID_SPACING).abs();
-    // Get closest one
-    let grid_distance = grid_distance_x.min(grid_distance_y);
-    // Make sure lines dont look ugly and appear on all screen sizes.
-    let grid_alpha = antialias(grid_distance, THICKNESS);
+    // Draw the grid
+    Grid::draw_grid(px_x, px_y, output);
 
-    // Same for highlights, but different scale
-    let highlight_distance_x = (px_x % (GRID_SPACING * HIGHLIGHT_SQUARES)).abs();
-    let highlight_distance_y = (px_y % (GRID_SPACING * HIGHLIGHT_SQUARES)).abs();
-    let highlight_distance = highlight_distance_x.min(highlight_distance_y);
-    let highlight_alpha = antialias(highlight_distance, THICKNESS);
+    // Drawing the vectors
+    Grid::draw_vectors(px_x, px_y, output);
+}
 
-    let axis_distance = px_x.abs().min(px_y.abs());
-    let axis_alpha = antialias(axis_distance, THICKNESS);
+pub struct Grid;
 
-    // Now the alpha channels are applied SEPERATLY to preserve the original alpha
-    // Lerp allows us to apply a mask with specific colors.
-    *output = output_color
-        .lerp(GRID_COLOR.xyz(), grid_alpha * GRID_COLOR.w)
-        .lerp(HIGHLIGHT_COLOR.xyz(), highlight_alpha * HIGHLIGHT_COLOR.w)
-        .lerp(AXIS_COLOR.xyz(), axis_alpha * AXIS_COLOR.w)
-        .extend(1.0);
+impl Grid {
+    pub fn draw_grid(px_x: f32, px_y: f32, output: &mut Vec4) {
+        let output_color = BG_COLOR.xyz();
+
+        // Get how many times spacing wraps.
+        let grid_distance_x = (px_x % GRID_SPACING).abs();
+        let grid_distance_y = (px_y % GRID_SPACING).abs();
+        // Get closest one
+        let grid_distance = grid_distance_x.min(grid_distance_y);
+        // Make sure lines dont look ugly and appear on all screen sizes.
+        let grid_alpha = antialias(grid_distance, THICKNESS);
+
+        // Same for highlights, but different scale
+        let highlight_distance_x = (px_x % (GRID_SPACING * HIGHLIGHT_SQUARES)).abs();
+        let highlight_distance_y = (px_y % (GRID_SPACING * HIGHLIGHT_SQUARES)).abs();
+        let highlight_distance = highlight_distance_x.min(highlight_distance_y);
+        let highlight_alpha = antialias(highlight_distance, THICKNESS);
+
+        let axis_distance = px_x.abs().min(px_y.abs());
+        let axis_alpha = antialias(axis_distance, THICKNESS);
+
+        // Now the alpha channels are applied SEPERATLY to preserve the original alpha
+        // Lerp allows us to apply a mask with specific colors.
+        *output = output_color
+            .lerp(GRID_COLOR.xyz(), grid_alpha * GRID_COLOR.w)
+            .lerp(HIGHLIGHT_COLOR.xyz(), highlight_alpha * HIGHLIGHT_COLOR.w)
+            .lerp(AXIS_COLOR.xyz(), axis_alpha * AXIS_COLOR.w)
+            .extend(1.0);
+    }
+
+    pub fn draw_vectors(px_x: f32, px_y: f32, output: &mut Vec4) {
+        let current_pos = Vec2::new(px_x, px_y);
+
+        let start_point = Vec2::new(
+            (px_x / GRID_SPACING).floor() * GRID_SPACING,
+            (px_y / GRID_SPACING).floor() * GRID_SPACING,
+        );
+
+        let vec = arrow_fn(start_point.x, start_point.y);
+        let relative_vec = start_point + vec;
+        let len = vec.length();
+
+        let line_sdf = Grid::sdf_line(start_point, relative_vec, current_pos);
+        let line_alpha = antialias_no_fwidth(line_sdf, ARROW_THICKNESS);
+
+        *output = output.lerp(AXIS_COLOR, line_alpha);
+    }
+
+    pub fn sdf_line(a_pos: Vec2, b_pos: Vec2, p_pos: Vec2) -> f32 {
+        let ab_vec = b_pos - a_pos;
+        let ap_vec = p_pos - a_pos;
+        // Originally:
+        // let h = ab_vec.dot(ap_vec) / ab_vec.length().powi(2);
+        // Much faster:
+        let h = ab_vec.dot(ap_vec) / ab_vec.dot(ab_vec).max(0.001);
+        let h_clamped = h.clamp(0.0, 1.0);
+        (ap_vec - h_clamped * ab_vec).length()
+    }
 }
 
 // Utilitiy functions
 
-pub fn antialias(dist: f32, thickness: f32) -> f32 {
+fn antialias_no_fwidth(dist: f32, thickness: f32) -> f32 {
+    let edge: f32 = 1.0;
+    1.0 - smoothstep(thickness - edge, thickness + edge, dist)
+}
+
+fn antialias(dist: f32, thickness: f32) -> f32 {
     let edge = dist.fwidth();
     1.0 - smoothstep(thickness - edge, thickness + edge, dist)
 }
